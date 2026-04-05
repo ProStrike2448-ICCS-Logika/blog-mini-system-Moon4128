a5c32edb2d4d7fcd8d50c00ceedda210bdb1de05
Moon4128 <167788546+Moon4128@users.noreply.github.com>
2026-04-05 15:22:20 +0300

    Initial commit with Django project

--- my_bloge/urls.py
+++ my_bloge/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import post_list
+from blog.views import post_list, post_detail, author_post_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
